2c5cb18f8a4cd35df5a22fc963d15094eee9e2c0
Abik1111 <abinashkarmacharya@gmail.com>
2020-09-16 22:50:40 +0545

    updates

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 (function (global) {
 
     var myNameSpc = {};
-    var threeTabResults="data/tab3.json";
+    var threeTabResults = "data/tab3.json";
     var searchResults = "data/data1.json";
     var searchResultsTitleHTML = "snippets/title.html";
     var searchResultsBodyHTML = "snippets/searchresult.html";
-  
+
     var insertHtml = function (selector, html) {
         var targetElem = document.querySelector(selector);
         targetElem.innerHTML = html;
@@ -17,23 +17,19 @@
     };
 
     document.addEventListener("DOMContentLoaded",
-    function(event){
-        myNameSpc.loadTabResults(0);
+        function (event) {
+            myNameSpc.loadTabResults(true);
 
-    });
+        });
 
-    myNameSpc.loadTabResults=function(tabType){
-        if(tabType===0){
-            console.log("HOT");
-            global.tab=0;
+    myNameSpc.loadTabResults = function (naya) {
+        if (naya === true) {
+            global.currentPage = 1;
+            console.log("naya ho!!");
         }
-        else if(tabType===1){
-            console.log("TOP");
-            global.tab=1;
-        }
-        else{
-            console.log("BEST");
-            global.tab=2;
+        else {
+            console.log("naya haina hai!!")
+            global.currentPage = naya;
         }
         $ajaxUtils.sendGetRequest(threeTabResults, buildAndShowTabResultsHTML);
 
@@ -43,26 +39,19 @@
         $ajaxUtils.sendGetRequest(searchResultsBodyHTML,
             function (searchResultsBodyHTML) {
                 var searchResultsViewHTML = buildTabViewHTML(results, searchResultsBodyHTML);
-                finalHTML="";
-                var tabNames=["Hot Properties","Top Viewed","Best Buys"]
-                for (var i=0;i<3;i++){
-                    if (global.tab===i){
-                        finalHTML+='<li role="presentation" class="active"><a onclick="$myNameSpc.loadTabResults('+i+')">'+tabNames[i]+'</a></li>';
-                    }
-                    else{
-                        finalHTML+='<li role="presentation"><a onclick="$myNameSpc.loadTabResults('+i+')">'+tabNames[i]+'</a></li>';
-                    }
-                }
-                insertHtml("#specific-list", finalHTML);
-                insertHtml("#property", searchResultsViewHTML)
-                
+                // finalHTML="";
+                insertHtml("#index-page", searchResultsViewHTML)
+
 
             }, false);
     }
 
     function buildTabViewHTML(results, searchResultsBodyHTML) {
-        finalHTML="";
+        var finalHTML = "";
+        finalHTML += '<div><h1 id="heading-1">Latest Properties</h1></div>';
+        finalHTML += '<div id="property" class="row">';
         var desiredProperties = results.desired_properties;
+        var numOfPages = results.num_of_pages;
         for (var i = 0; i < desiredProperties.length; i++) {
             var html = searchResultsBodyHTML;
             html = insertProperty(html, "image", desiredProperties[i].image);
@@ -72,18 +61,20 @@
             html = insertProperty(html, "Price", desiredProperties[i].Price);
             finalHTML += html;
         }
-        finalHTML += "</div>"
+        finalHTML += "</div>";
+        pagination = insertPagination(numOfPages, "$myNameSpc.loadTabResults");
+        finalHTML += pagination;
         return finalHTML;
-    } 
+    }
 
     myNameSpc.loadSearchData = function (naya) {
-        if(naya===true){
-            global.currentPage=1;
+        if (naya === true) {
+            global.currentPage = 1;
             console.log("naya ho!!");
         }
-        else{
+        else {
             console.log("naya haina hai!!")
-            global.currentPage=naya;
+            global.currentPage = naya;
         }
         $ajaxUtils.sendGetRequest(searchResults, buildAndShowResultsHTML);
     };
@@ -108,7 +99,7 @@
         var finalHTML = searchResultsTitleHTML;
         finalHTML += "<div id='property' class='row'>";
         var desiredProperties = results.desired_properties;
-        var numOfPages=results.num_of_pages;
+        var numOfPages = results.num_of_pages;
         for (var i = 0; i < desiredProperties.length; i++) {
             var html = searchResultsBodyHTML;
             html = insertProperty(html, "image", desiredProperties[i].image);
@@ -118,51 +109,115 @@
             html = insertProperty(html, "Price", desiredProperties[i].Price);
             finalHTML += html;
         }
-        finalHTML += "</div>";       
-        pagination=insertPagination(numOfPages);
-        finalHTML+=pagination;
+        finalHTML += "</div>";
+        pagination = insertPagination(numOfPages, "$myNameSpc.loadSearchData");
+        finalHTML += pagination;
         return finalHTML
     }
-    function insertPagination(numOfPages){
-        finalHTML="";
-        finalHTML+='<div class="center">';
-        finalHTML+="<div class='pagination'>";
-        if (global.currentPage>1){
-            pageString='<a href="#" onclick="$myNameSpc.loadSearchData({{i}})">&laquo;</a>';
-            pageString=insertProperty(pageString, "i", (global.currentPage-1));
-            
+    function insertPagination(numOfPages, functionx) {
+        console.log(numOfPages);
+        var finalHTML;
+        finalHTML = "";
+        finalHTML = '<div id="page-number" class="row">';
+        finalHTML += '<div class="center">';
+        finalHTML += "<div class='pagination'>";
+        if (global.currentPage > 1) {
+            pageString = '<a href="#" onclick="{{function}}({{i}})">&laquo;</a>';
+            pageString = insertProperty(pageString, "function", functionx);
+            pageString = insertProperty(pageString, "i", (global.currentPage - 1));
+
         }
-        else{
-            pageString='<a href="#" onclick="$myNameSpc.loadSearchData(1)">&laquo;</a>';
+        else {
+            pageString = '<a onclick="{{function}}(1)">&laquo;</a>';
+            pageString = insertProperty(pageString, "function", functionx)
         }
-        finalHTML+=pageString;
-        for (var i=0;i<numOfPages;i++){
-            if ((i+1)==global.currentPage){
-                pageString='<a href="#" onclick="$myNameSpc.loadSearchData({{i}})" class="active">{{i}}</a>';
-                pageString=insertProperty(pageString, "i", i+1);
+        finalHTML += pageString;
+        if (numOfPages < 10) {
+            for (var i = 0; i < numOfPages; i++) {
+                if ((i + 1) == global.currentPage) {
+                    pageString = '<a href="#" onclick="{{function}}({{i}})" class="active">{{i}}</a>';
+                    pageString = insertProperty(pageString, "i", i + 1);
+                    pageString = insertProperty(pageString, "function", functionx);
+                }
+                else {
+                    pageString = '<a href="#" onclick="{{function}}({{i}})">{{i}}</a>';
+                    pageString = insertProperty(pageString, "i", i + 1);
+                    pageString = insertProperty(pageString, "function", functionx);
+
+                }
+                finalHTML += pageString;
+            }
+        }
+        else {
+            if (global.currentPage - 1 >= 2 && global.currentPage - numOfPages <= -2) {
+                for (var i = global.currentPage - 2; i <= global.currentPage + 2; i++) {
+                    if (i == global.currentPage) {
+                        pageString = '<a href="#" onclick="{{function}}({{i}})" class="active">{{i}}</a>';
+                        pageString = insertProperty(pageString, "i", i);
+                        pageString = insertProperty(pageString, "function", functionx);
+                    }
+                    else {
+                        pageString = '<a href="#" onclick="{{function}}({{i}})">{{i}}</a>';
+                        pageString = insertProperty(pageString, "i", i);
+                        pageString = insertProperty(pageString, "function", functionx);
+
+                    }
+                    finalHTML+=pageString;
+                }
+            }
+            else if (global.currentPage - 1 < 2) {
+                for (var i = 1; i <= 5; i++) {
+                    if (i == global.currentPage) {
+                        pageString = '<a href="#" onclick="{{function}}({{i}})" class="active">{{i}}</a>';
+                        pageString = insertProperty(pageString, "i", i);
+                        pageString = insertProperty(pageString, "function", functionx);
+                    }
+                    else {
+                        pageString = '<a href="#" onclick="{{function}}({{i}})">{{i}}</a>';
+                        pageString = insertProperty(pageString, "i", i);
+                        pageString = insertProperty(pageString, "function", functionx);
+
+                    }
+                    finalHTML+=pageString;
+                }
             }
             else{
-                pageString='<a href="#" onclick="$myNameSpc.loadSearchData({{i}})">{{i}}</a>';
-                pageString=insertProperty(pageString, "i", i+1);
+                for(var i =numOfPages-4;i<=numOfPages;i++){
+                    if (i==global.currentPage){
+                        pageString='<a href="#" onclick="{{function}}({{i}})" class="active">{{i}}</a>';
+                        pageString=insertProperty(pageString, "i", i);
+                        pageString=insertProperty(pageString, "function", functionx);
+                    }
+                    else{
+                        pageString='<a href="#" onclick="{{function}}({{i}})">{{i}}</a>';
+                        pageString=insertProperty(pageString, "i", i);
+                        pageString=insertProperty(pageString, "function", functionx);
+        
+                    }
+                    finalHTML+=pageString;
+               }
             }
-            finalHTML+=pageString;
         }
-        if (global.currentPage<numOfPages){
-            pageString='<a href="#" onclick="$myNameSpc.loadSearchData({{i}})">&raquo;</a>';
-            pageString=insertProperty(pageString, "i", (global.currentPage+1));
-            
+        if (global.currentPage < numOfPages) {
+            pageString = '<a href="#" onclick="{{function}}({{i}})">&raquo;</a>';
+            pageString = insertProperty(pageString, "i", (global.currentPage + 1));
+            pageString = insertProperty(pageString, "function", functionx);
+
+
         }
-        else{
-            pageString='<a href="#" onclick="$myNameSpc.loadSearchData({{i}})">&laquo;</a>';
-            pageString=insertProperty(pageString, "i", numOfPages);
+        else {
+            pageString = '<a onclick="{{function}}({{i}})">&raquo;</a>';
+            pageString = insertProperty(pageString, "i", numOfPages);
+            pageString = insertProperty(pageString, "function", functionx);
+
         }
-        finalHTML+=pageString;
-        finalHTML += "</div></div>";
+        finalHTML += pageString;
+        finalHTML += "</div></div></div>";
         return finalHTML;
     }
-    myNameSpc.changeDropdown=function(typeOfLand){
-        text=typeOfLand+'<span class="caret"></span>';
-        insertHtml('#dropdown-btn',text);
+    myNameSpc.changeDropdown = function (typeOfLand) {
+        text = typeOfLand + '<span class="caret"></span>';
+        insertHtml('#dropdown-btn', text);
     }
     global.$myNameSpc = myNameSpc;
 })(window);
