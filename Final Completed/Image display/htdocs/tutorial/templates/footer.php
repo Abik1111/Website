bdc21c077857257206392187230d05d5bc7f931a
Dilip Rana <dilipRana@users.noreply.github.com>
2020-09-18 14:14:35 +0545

    d

--- Final Completed/Image display/htdocs/tutorial/templates/footer.php
+++
@@ -1,5 +0,0 @@
-
- <footer class = "section">
- 	<div class = "center grey-text">Copyright 2020 Ninja Pizza</div>
- </footer>
-</body>
--- Final Completed/test.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	$array = "1 2 3 4 5 6 ";
-	$string = str_replace("5 ", "", $array);
-	print_r(explode("/",$string));
- ?>
