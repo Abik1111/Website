b2085b83f1da99220a9af8eedae8276b077371b1
Dilip Rana <dilipRana@users.noreply.github.com>
2020-09-18 14:19:31 +0545

    c

---
+++ FInal Complete/js/script.js
@@ -0,0 +1,230 @@
+(function (global) {
+
+    var myNameSpc = {};
+    var threeTabResults = "data/tab3.json";
+    var searchResults = "data/data1.json";
+    var searchResultsTitleHTML = "snippets/title.html";
+    var searchResultsBodyHTML = "snippets/searchresult.html";
+
+    var insertHtml = function (selector, html) {
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHTML = html;
+    };
+    var insertProperty = function (string, propName, propValue) {
+        var propToReplace = "{{" + propName + "}}";
+        string = string.replace(new RegExp(propToReplace, "g"), propValue);
+        return string;
+    };
+
+    document.addEventListener("DOMContentLoaded",
+        function (event) {
+            myNameSpc.loadTabResults(true);
+
+        });
+        
+    myNameSpc.loadTabResults = function (naya) {
+        // if (naya === true) {
+        //     global.currentPage = 1;
+        //     console.log("naya ho!!");
+        // }
+        // else {
+        //     console.log("naya haina hai!!")
+        //     global.currentPage = naya;
+        // }
+        $ajaxUtils.sendGetRequest(threeTabResults, buildAndShowTabResultsHTML);
+
+    }
+
+    function buildAndShowTabResultsHTML(results) {
+        $ajaxUtils.sendGetRequest(searchResultsBodyHTML,
+            function (searchResultsBodyHTML) {
+                var searchResultsViewHTML = buildTabViewHTML(results, searchResultsBodyHTML);
+                // finalHTML="";
+                insertHtml("#index-page", searchResultsViewHTML)
+
+
+            }, false);
+    }
+
+    function buildTabViewHTML(results, searchResultsBodyHTML) {
+        var finalHTML = "";
+        
+        finalHTML += '<div id="property" class="row">';
+        var desiredProperties = results.desired_properties;
+        var numOfPages = results.num_of_pages;
+        var currentPage=results.current_page;
+
+        for (var i = 0; i < desiredProperties.length; i++) {
+            var html = searchResultsBodyHTML;
+            html = insertProperty(html, "image", desiredProperties[i].image);
+            html = insertProperty(html, "id", desiredProperties[i].id);
+            html = insertProperty(html, "Area", desiredProperties[i].Area);
+            html = insertProperty(html, "Location", desiredProperties[i].Location);
+            html = insertProperty(html, "Price", desiredProperties[i].Price);
+            finalHTML += html;
+        }
+        finalHTML += "</div>";
+        pagination = insertPagination(currentPage, numOfPages, "$myNameSpc.loadTabResults");
+        finalHTML += pagination;
+        return finalHTML;
+    }
+
+    myNameSpc.loadSearchData = function (naya) {
+        if (naya === true) {
+            global.currentPage = 1;
+            console.log("naya ho!!");
+        }
+        else {
+            console.log("naya haina hai!!")
+            global.currentPage = naya;
+        }
+        $ajaxUtils.sendGetRequest(searchResults, buildAndShowResultsHTML);
+    };
+    myNameSpc.loadDataDetails = function (id) {
+        $ajaxUtils.sendGetRequest("data/" + id + ".html", function (responseText) {
+            insertHtml("#main-content", responseText)
+        }, false);
+    };
+
+    function buildAndShowResultsHTML(results) {
+        $ajaxUtils.sendGetRequest(searchResultsTitleHTML,
+            function (searchResultsTitleHTML) {
+                $ajaxUtils.sendGetRequest(searchResultsBodyHTML,
+                    function (searchResultsBodyHTML) {
+                        var searchResultsViewHTML = buildResultsViewHTML(results, searchResultsTitleHTML, searchResultsBodyHTML);
+                        insertHtml("#main-content", searchResultsViewHTML)
+                    }, false);
+            }, false);
+    }
+
+    function buildResultsViewHTML(results, searchResultsTitleHTML, searchResultsBodyHTML) {
+        var finalHTML = searchResultsTitleHTML;
+        finalHTML += "<div id='property' class='row'>";
+        var desiredProperties = results.desired_properties;
+        var numOfPages = results.num_of_pages;
+        var currentPage=results.current_page;
+        for (var i = 0; i < desiredProperties.length; i++) {
+            var html = searchResultsBodyHTML;
+            html = insertProperty(html, "image", desiredProperties[i].image);
+            html = insertProperty(html, "id", desiredProperties[i].id);
+            html = insertProperty(html, "Area", desiredProperties[i].Area);
+            html = insertProperty(html, "Location", desiredProperties[i].Location);
+            html = insertProperty(html, "Price", desiredProperties[i].Price);
+            finalHTML += html;
+        }
+        finalHTML += "</div>";
+        console.log("Idiot");
+        console.log(numOfPages);
+        console.log(currentPage);
+        pagination = insertPagination(currentPage, numOfPages, "$myNameSpc.loadSearchData");
+        finalHTML += pagination;
+        return finalHTML
+    }
+    function insertPagination(currentPage, numOfPages, functionx) {
+        console.log(numOfPages);
+        console.log(currentPage);
+        var finalHTML;
+        finalHTML = "";
+        finalHTML = '<div id="page-number" class="row">';
+        finalHTML += '<div class="center">';
+        finalHTML += "<div class='pagination'>";
+        if (currentPage > 1) {
+            pageString = '<a href="?page_number={{i}}" onclick="{{function}}({{i}})">&laquo;</a>';
+            pageString = insertProperty(pageString, "function", functionx);
+            pageString = insertProperty(pageString, "i", (currentPage - 1));
+
+        }
+        else {
+            pageString = '<a href="?page_number={{i}}" onclick="{{function}}(1)">&laquo;</a>';
+            pageString = insertProperty(pageString, "function", functionx)
+        }
+        finalHTML += pageString;
+        if (numOfPages < 6) {
+            for (var i = 0; i < numOfPages; i++) {
+                if ((i + 1) == currentPage) {
+                    pageString = '<a href="?page_number={{i}}" onclick="{{function}}({{i}})" class="active">{{i}}</a>';
+                    pageString = insertProperty(pageString, "i", i + 1);
+                    pageString = insertProperty(pageString, "function", functionx);
+                }
+                else {
+                    pageString = '<a href="?page_number={{i}}" onclick="{{function}}({{i}})">{{i}}</a>';
+                    pageString = insertProperty(pageString, "i", i + 1);
+                    pageString = insertProperty(pageString, "function", functionx);
+
+                }
+                finalHTML += pageString;
+            }
+        }
+        else {
+            if (currentPage - 1 >= 2 && currentPage - numOfPages <= -2) {
+                for (var i = currentPage - 2; i <= currentPage + 2; i++) {
+                    if (i == currentPage) {
+                        pageString = '<a href="?page_number={{i}}" onclick="{{function}}({{i}})" class="active">{{i}}</a>';
+                        pageString = insertProperty(pageString, "i", i);
+                        pageString = insertProperty(pageString, "function", functionx);
+                    }
+                    else {
+                        pageString = '<a href="?page_number={{i}}" onclick="{{function}}({{i}})">{{i}}</a>';
+                        pageString = insertProperty(pageString, "i", i);
+                        pageString = insertProperty(pageString, "function", functionx);
+
+                    }
+                    finalHTML+=pageString;
+                }
+            }
+            else if (currentPage - 1 < 2) {
+                for (var i = 1; i <= 5; i++) {
+                    if (i == currentPage) {
+                        pageString = '<a href="?page_number={{i}}" onclick="{{function}}({{i}})" class="active">{{i}}</a>';
+                        pageString = insertProperty(pageString, "i", i);
+                        pageString = insertProperty(pageString, "function", functionx);
+                    }
+                    else {
+                        pageString = '<a href="?page_number={{i}}" onclick="{{function}}({{i}})">{{i}}</a>';
+                        pageString = insertProperty(pageString, "i", i);
+                        pageString = insertProperty(pageString, "function", functionx);
+
+                    }
+                    finalHTML+=pageString;
+                }
+            }
+            else{
+                for(var i =numOfPages-4;i<=numOfPages;i++){
+                    if (i==currentPage){
+                        pageString='<a href="?page_number={{i}}" onclick="{{function}}({{i}})" class="active">{{i}}</a>';
+                        pageString=insertProperty(pageString, "i", i);
+                        pageString=insertProperty(pageString, "function", functionx);
+                    }
+                    else{
+                        pageString='<a href="?page_number={{i}}" onclick="{{function}}({{i}})">{{i}}</a>';
+                        pageString=insertProperty(pageString, "i", i);
+                        pageString=insertProperty(pageString, "function", functionx);
+        
+                    }
+                    finalHTML+=pageString;
+               }
+            }
+        }
+        if (currentPage < numOfPages) {
+            pageString = '<a href="?page_number={{i}}" onclick="{{function}}({{i}})">&raquo;</a>';
+            pageString = insertProperty(pageString, "i", (currentPage + 1));
+            pageString = insertProperty(pageString, "function", functionx);
+
+
+        }
+        else {
+            pageString = '<a href="?page_number={{i}}" onclick="{{function}}({{i}})">&raquo;</a>';
+            pageString = insertProperty(pageString, "i", numOfPages);
+            pageString = insertProperty(pageString, "function", functionx);
+
+        }
+        finalHTML += pageString;
+        finalHTML += "</div></div></div>";
+        return finalHTML;
+    }
+    myNameSpc.changeDropdown = function (typeOfLand) {
+        text = typeOfLand + '<span class="caret"></span>';
+        insertHtml('#dropdown-btn', text);
+    }
+    global.$myNameSpc = myNameSpc;
+})(window);
---
+++ FInal Complete/multiverse/server_database.php
@@ -0,0 +1,313 @@
+<?php 
+	
+
+	/**
+		***Documentation***
+
+		class Server
+			__construct(String host,String username,String password);
+			boolean createDatabase(String datbase_name);
+			boolean dropDatabase(String database_name);
+			connection getConnection();
+			String getError();
+		
+		class Database
+			__construct(String host,String username,String password,String database_name);
+			boolean createTable(String table_name,String columns_and_columns_type);//columns include sql command
+				e.g:- columns_and_columns_type = "id INT AUTO_INCREMENT,name CHAR(90) NOT NULL,job CHAR(90) NOT NULL,primary key(id)";
+			boolean dropTable(String table_name);
+			boolean insert(String table_name,Associative_array_1D datas);
+				e.g:- datas = {'name'='Sparrow','job'='pirates'};
+			Associative_array_2D select(String table_name,Array_1D fields=null,String conditions="true");conditions include sql command
+				e.g:- fields = {'name','job'};
+				e.g:- conditions = "id = '0' AND job = 'pirates'";
+				output datas as e.g:- datas = {{'name'='Sparrow','job'='pirates'}};
+			boolean update(String table_name,Associative_array_1D datas,String conditions="true");conditions include sql command
+				e.g:- datas = {'name'='Sparrow','job'='pirates'};
+				e.g:- conditions = "id = '0' AND job = 'pirates'";
+			boolean delete(String table_name,String conditions="true");conditions include sql command
+				e.g:- conditions = "id = '0' AND job = 'pirates'";
+			connection getConnection();//for good practice at end of program close connections
+				e.g:- connection conn = database.getConnection();
+							//TODO
+							mysqli_close(conn);
+			String getError();
+
+	**/
+
+
+
+	class Server{
+		private $host;
+		private $username;
+		private $password;
+		private $errors;
+
+		private function connect(){
+			return mysqli_connect($this->host,$this->username,$this->password);
+		}
+
+		private function close($connection){
+			mysqli_close($connection);
+		}
+
+		public function __construct($host,$username,$password){
+			$this->host = $host;
+			$this->username = $username;
+			$this->password = $password;			
+		}
+
+		public function createDatabase($database_name){
+			$connection = $this->connect();
+			if(!$connection){
+				$errors ="Connection Error".mysqli_connect_error();
+				return false;
+			}
+
+			$sql = "CREATE DATABASE IF NOT EXISTS ".$database_name;
+
+			if(!mysqli_query($connection,$sql)){
+				$errors = "Error creating database: " . mysqli_error($connection);
+				$this->close(connection);
+				return false;
+			}
+			$errors="";
+			$this->close($connection);
+			return true;
+		}
+
+		public function dropDatabase($database_name){
+			$connection = $this->connect();
+			if(!$connection){
+				$errors ="Connection Error".mysql_connect_error();
+				return false;
+			}
+
+			$sql = "DROP DATABASE ".$database_name;
+
+			if(!mysqli_query($connection,$sql)){
+				$errors = "Error deleting record: " . mysqli_error($connection);
+				$this->close($connection);
+				return false;
+			}
+			$errors="";
+			$this->close($connection);
+			return true;
+		}
+
+		public function getConnection(){
+			$connection = $this->connect();
+			if(!$connection){
+				$errors ="Connection Error".mysqli_connect_error();
+				return null;
+			}
+			return $connection;
+		}
+
+		public function getErrors(){
+			return $errors;
+		}
+	}
+
+	class Database{
+		private $host;
+		private $username;
+		private $password;
+		private $database_name;
+		private $errors;
+
+		private function connect(){
+			return mysqli_connect($this->host,$this->username,$this->password,$this->database_name);
+		}
+
+		private function close($connection){
+			mysqli_close($connection);
+		}		
+
+		public function __construct($host,$username,$password,$database_name){
+			$this->host = $host;
+			$this->username = $username;
+			$this->password = $password;
+			$this->database_name = $database_name;
+		}
+
+		public function createTable($table_name,$columns){
+			$connection = $this->connect();
+			if(!$connection){
+				$errors ="Connection Error".mysqli_connect_error();
+				return false;
+			}
+
+			$sql = "SELECT 1 FROM ".$table_name;
+			$exist =  mysqli_query($connection,$sql);
+			if($exist != false){
+				$errors = "Already Exist: " ;
+				return false;
+			}
+
+			$sql = "CREATE TABLE ".$table_name."(".$columns.")";
+
+			if(!mysqli_query($connection,$sql)){
+				$errors = "Error creating table: " . mysqli_error($connection);
+				$this->close($connection);
+				return false;
+			}
+			$errors="";
+			$this->close($connection);
+			return true;
+		}
+
+		public function dropTable($table_name){
+			$connection = $this->connect();
+			if(!$connection){
+				$errors ="Connection Error".mysqli_connect_error();
+				return false;
+			}
+
+			$sql = "DROP TABLE ".$table_name;
+
+			if(!mysqli_query($connection,$sql)){
+				$errors = "Error deleting table: " . mysqli_error($connection);
+				$this->close($connection);
+				return false;
+			}
+			$errors="";
+			$this->close($connection);
+			return true;
+		}
+
+		public function insert($table_name,$datas_associative_array){
+			$connection = $this->connect();
+			if(!$connection){
+				$errors ="Connection Error".mysqli_connect_error();
+				return false;
+			}
+
+			$fields = implode(", ", array_keys($datas_associative_array));
+			$values = array_values($datas_associative_array);
+			for($i=0;$i<count($values);$i++){
+				$values[$i] = "'$values[$i]'";
+			}
+			$values =  implode(", ", $values);
+
+			$sql = "INSERT INTO ".$table_name."(".$fields.") VALUES (".$values.")";
+
+			if(!mysqli_query($connection,$sql)){
+				$errors = "Error inserting datas: " . mysqli_error($connection);
+				$this->close($connection);
+				return false;
+			}
+			$errors="";
+			$this->close($connection);
+			return true;
+		}
+
+		public function select($table_name,$fields_array=null,$conditions=null, $order=null, $limit=null, $offset=null){
+			$connection = $this->connect();
+			if(!$connection){
+				$errors ="Connection Error".mysqli_connect_error();
+				return null;
+			}
+
+			if($fields_array==null){
+				$sql = "SELECT * FROM ".$table_name;
+			}else{
+				$fields = implode(", ",$fields_array);
+				$sql = "SELECT ".$fields." FROM ".$table_name;
+			}
+
+<<<<<<< HEAD:htdocs/multiverse/server_database.php
+			if($conditions!="true"){
+				$sql=$sql." WHERE ".$conditions;
+=======
+			if($conditions!=null){
+				$sql .= " WHERE ".$conditions;
+			}
+			
+			if($order!=null){
+				$sql .=" ORDER BY ".$order;
+			}
+			
+			if($limit!=null){
+				$sql .=" LIMIT ".$limit;
+			}
+			
+			if($offset!=null){
+				$sql .=" OFFSET ".$offset;
+>>>>>>> eb8469966c6027d05b490a0beb5be81def804ad6:htdocs/lib/server_database.php
+			}
+
+			$result = mysqli_query($connection,$sql);
+			if(!$result){
+				$this->errors ="Query Error".mysqli_error($connection);
+				return null;
+			}
+
+			$datas = mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+			mysqli_free_result($result);
+			
+			$this->close($connection);
+			
+			return $datas;
+		}
+
+		public function update($table_name,$datas_associative_array,$conditions="true"){
+			$connection = $this->connect();
+			if(!$connection){
+				$errors ="Connection Error".mysqli_connect_error();
+				return false;
+			}
+			$fields = array_keys($datas_associative_array);
+			$values = array_values($datas_associative_array);
+			for($i=0;$i<count($fields);$i++){
+				$values[$i] = $fields[$i]."="."'".$values[$i]."'";
+			}
+			$values =  implode(", ", $values);
+
+			$sql = " UPDATE ".$table_name." SET ".$values." WHERE ".$conditions;
+
+			if(!mysqli_query($connection,$sql)){
+				$errors = "Error updating datas: " . mysqli_error($connection);
+				$this->close($connection);
+				return false;
+			}
+			$errors="";
+			$this->close($connection);
+			return true;
+		}
+
+		public function delete($table_name,$conditions="true"){
+			$connection = $this->connect();
+			if(!$connection){
+				$errors ="Connection Error".mysqli_connect_error();
+				return false;
+			}
+
+			$sql = " DELETE FROM ".$table_name." WHERE ".$conditions;
+
+			if(!mysqli_query($connection,$sql)){
+				$errors = "Error Deleting datas: " . mysqli_error($connection);
+				$this->close($connection);
+				return false;
+			}
+			$errors="";
+			$this->close($connection);
+			return true;
+		}
+
+		public function getConnection(){
+			$connection = $this->connect();
+			if(!$connection){
+				$errors ="Connection Error".mysqli_connect_error();
+				return null;
+			}
+			return $connection;
+		}
+
+		public function getErrors(){
+			return $errors;
+		}
+	}
+
+ ?>
